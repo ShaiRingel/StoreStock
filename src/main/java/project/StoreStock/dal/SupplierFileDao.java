27ebc13be885c04ac71531d910200344d68a0c50
Ravidddddje <91815587+Ravidddddje@users.noreply.github.com>
2025-12-23 06:31:47 +0200

    Update SupplierFileDao.java

--- src/main/java/project/StoreStock/dal/SupplierFileDao.java
+++ src/main/java/project/StoreStock/dal/SupplierFileDao.java
@@ -1,1 +1,67 @@
+package project.StoreStock.dal;
 
+import org.springframework.stereotype.Repository;
+import project.StoreStock.entity.Supplier;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class SupplierFileDao {
+
+    private final String FILENAME = "suppliers.dat";
+
+    public List<Supplier> getAll() throws IOException, ClassNotFoundException {
+        File file = new File(FILENAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            List<Supplier> suppliers = (List<Supplier>) ois.readObject();
+            Collections.sort(suppliers);
+            return suppliers;
+        }
+    }
+
+    public void save(Supplier supplier) throws IOException, ClassNotFoundException {
+        List<Supplier> suppliers = getAll();
+        suppliers.add(supplier);
+        writeToFile(suppliers);
+    }
+
+    public void update(Supplier supplier) throws IOException, ClassNotFoundException {
+        List<Supplier> suppliers = getAll();
+        for (int i = 0; i < suppliers.size(); i++) {
+            if (suppliers.get(i).getId().equals(supplier.getId())) {
+                suppliers.set(i, supplier);
+                break;
+            }
+        }
+        writeToFile(suppliers);
+    }
+
+    public void delete(String id) throws IOException, ClassNotFoundException {
+        List<Supplier> suppliers = getAll();
+        suppliers.removeIf(s -> s.getId().equals(id));
+        writeToFile(suppliers);
+    }
+
+    public Supplier get(String id) throws IOException, ClassNotFoundException {
+        List<Supplier> suppliers = getAll();
+        for (Supplier s : suppliers) {
+            if (s.getId().equals(id)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private void writeToFile(List<Supplier> suppliers) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILENAME))) {
+            oos.writeObject(suppliers);
+        }
+    }
+}
