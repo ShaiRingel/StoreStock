549c43d9d7c36f20862a16bf0fcc0d01ec8cd307
nikaa <nika.noa07@gmail.com>
2026-01-24 18:55:15 +0200

    Add files via upload

--- src/main/java/project/StoreStock/controller/EntityController.java
+++ src/main/java/project/StoreStock/controller/EntityController.java
@@ -1,9 +1,12 @@
 package project.StoreStock.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import project.StoreStock.entity.Product;
@@ -18,7 +21,9 @@ import java.util.List;
 @Controller
 public class EntityController {
 
+    @Autowired
     private final ProductService productService;
+    @Autowired
     private final SupplierService supplierService;
 
     @Autowired
@@ -116,5 +121,94 @@ public class EntityController {
 
         return "redirect:/" + page;
     }
+
+    @GetMapping("/products-form")
+    public String showProductForm(@RequestParam(value = "id", required = false) Integer id, Model model) {
+        Product product;
+
+        if(id == null){
+            product = new Product();
+            product.setSupplier(new Supplier());
+        }
+        else {
+            try {
+                product = productService.get(id);
+                if(product == null)
+                    return "redirect:/products?error=notfound";
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }
+        }
+        model.addAttribute("product",product);
+        try {
+            model.addAttribute("suppliers", supplierService.getAll());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "products-form";
+    }
+
+    @PostMapping("/save-product")
+    public String saveProduct(@Valid @ModelAttribute("product") Product product,
+                              BindingResult result, Model model) {
+            if (result.hasErrors()) {
+                try {
+                    model.addAttribute("suppliers", supplierService.getAll());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                return "products-form";
+            }
+            try {
+                Supplier supplier = supplierService.get(product.getSupplier().getId());
+                product.setSupplier(supplier);
+                try{
+                    if (productService.get(product.getId()) == null)
+                        productService.save(product);
+                    else
+                        productService.update(product);
+                }catch(Exception er){
+                        throw new RuntimeException(er);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return "redirect:/mainScreen";
+        }
+
+    @GetMapping("/suppliers-form")
+    public String showSupplierForm(@RequestParam(value = "id", required = false) Integer id, Model model) {
+        Supplier supplier;
+        if(id == null)
+            supplier = new Supplier();
+        else {
+            try{
+                supplier = supplierService.get(id);
+                if(supplier == null)
+                    return "redirect:/suppliers?error=notfound";
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        model.addAttribute("supplier",supplier);
+        return "suppliers-form";
+    }
+
+    @PostMapping("/save-supplier")
+    public String saveSupplier(@Valid @ModelAttribute("supplier") Supplier supplier,
+                              BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "suppliers-form";
+        try {
+            if (supplierService.get(supplier.getId()) == null)
+                supplierService.save(supplier);
+            else
+                supplierService.update(supplier);
+        } catch (Exception er) {
+            throw new RuntimeException(er);
+        }
+        return "redirect:/mainScreen";
+    }
+
 
 }
